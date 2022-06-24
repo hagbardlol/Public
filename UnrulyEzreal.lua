c9ae558ee01b281917322a6f86796efa596afced
hagbardlol <59706487+hagbardlol@users.noreply.github.com>
2022-06-24 01:19:08 -0800

    Update UnrulyEzreal.lua

--- UnrulyEzreal.lua
+++ UnrulyEzreal.lua
@@ -6,7 +6,7 @@ if Player.CharName ~= "Ezreal" then return end
 
 module("Unruly Ezreal", package.seeall, log.setup)
 clean.module("Unruly Ezreal", clean.seeall, log.setup)
-CoreEx.AutoUpdate("https://github.com/hagbardlol/Public/raw/main/UnrulyEzreal.lua", "1.0.2")
+CoreEx.AutoUpdate("https://raw.githubusercontent.com/hagbardlol/Public/main/UnrulyEzreal.lua", "1.0.3")
 
 local clock = os.clock
 local insert, sort = table.insert, table.sort
