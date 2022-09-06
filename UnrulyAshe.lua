b2b5432006e073fbdf50b7aa6a884ff6f564bfba
hagbardlol <>
2022-09-05 22:57:57 -0400

    Reupload UnrulyAshe

--- UnrulyAshe.lua
+++ UnrulyAshe.lua
@@ -1,85 +1,187 @@
+--[[
+    First Release By Thorn @ 20.Sept.2020
+]]
+
 if Player.CharName ~= "Ashe" then return end
 
-module("UnrulyAshe", package.seeall, log.setup)
-clean.module("UnrulyAshe", clean.seeall, log.setup)
-CoreEx.AutoUpdate("https://raw.githubusercontent.com/hagbardlol/Public/main/UnrulyAshe.lua", "1.0.4")
-local CoreEx = _G.CoreEx
-local Enums, EventManager, Renderer, Game, Vector = CoreEx.Enums, CoreEx.EventManager, CoreEx.Renderer, CoreEx.Game, CoreEx.Geometry.Vector
-local Screen = Renderer.GetResolution()
+module("Unruly Ashe", package.seeall, log.setup)
+clean.module("Unruly Ashe", clean.seeall, log.setup)
+local _version = "1.0.5"
+CoreEx.AutoUpdate("https://raw.githubusercontent.com/hagbardlol/Public/main/UnrulyAshe.lua", _version)
 
-local Text = {
-    Title = "Unfortunately, this assembly is no longer supported by us.",
-    Subtitle = "No need to panic! We have made a new assembly available for you. Just check the forum or download the new assembly with the in-game downloader.",
-    Message = "Please disable this assembly in your loader and press F5 to make the message disappear!",
-}
+local clock = os.clock
+local insert = table.insert
 
-local Size = {
-    Border = Vector(800, 230), -- width, height
-    Background = Vector(799, 228),
-    Logo = Vector(350, 100),
-    Warning = Vector(40, 40),
-    Text = {
-        Title = Renderer.CalcTextSize(Text.Title),
-        Subtitle = Renderer.CalcTextSize(Text.Subtitle),
-        Message = Renderer.CalcTextSize(Text.Message),
-    }
-}
+local _SDK = _G.CoreEx
+local Console, ObjManager, EventManager, Geometry, Input, Renderer, Enums, Game = _SDK.Console, _SDK.ObjectManager, _SDK.EventManager, _SDK.Geometry, _SDK.Input, _SDK.Renderer, _SDK.Enums, _SDK.Game
+local Menu, Orbwalker, Collision, Prediction, HealthPred = _G.Libs.NewMenu, _G.Libs.Orbwalker, _G.Libs.CollisionLib, _G.Libs.Prediction, _G.Libs.HealthPred
+local Spell = _G.Libs.Spell
 
-local Position = {
-    Border = Vector((Screen.x / 2) - Size.Border.x / 2, (Screen.y / 2) - Size.Border.y / 2),
-    Background = Vector((Screen.x / 2) - Size.Background.x / 2, (Screen.y / 2) - Size.Background.y / 2),
-    Logo = Vector(Screen.x / 2, Screen.y / 2 - Size.Logo.y / 2),
-    Line = {
-        Start = Vector((Screen.x / 2 - Size.Border.x / 2) + 10, (Screen.y / 2 - Size.Logo.y / 2) + 60),
-        End =  Vector((Screen.x / 2 + Size.Border.x / 2) - 10, (Screen.y / 2 - Size.Logo.y / 2) + 60)
-    },
-    Text = {
-        Title = Vector((Screen.x / 2) - Size.Text.Title.x / 2, (Screen.y / 2 - Size.Logo.y / 2) + 60 + Size.Text.Title.y / 2),
-        Subtitle = Vector((Screen.x / 2) - Size.Text.Subtitle.x / 2, (Screen.y / 2 - Size.Logo.y / 2) + 60 + Size.Text.Title.y + Size.Text.Subtitle.y / 2),
-        Message = Vector((Screen.x / 2) - Size.Text.Message.x / 2, (Screen.y / 2 - Size.Logo.y / 2) + 60 + Size.Text.Title.y + Size.Text.Subtitle.y + Size.Text.Message.y / 2),
-    },
-    Warning = Vector((Screen.x / 2) - Size.Warning.x / 2, (Screen.y / 2 - Size.Logo.y / 2) + 60 + Size.Text.Title.y + Size.Text.Subtitle.y + Size.Text.Message.y + Size.Warning.y / 2 + 10)
-}
+local _Q = Spell.Active({
+    Slot = Enums.SpellSlots.Q,
+    Delay = 0,
+    Speed = 2500
+})
+local _W = Spell.Skillshot({
+    Slot = Enums.SpellSlots.W,
+    Range = 1300,
+    Delay = 0.25,
+    Speed = 1500,
+    Radius = 20,
+    Type = "Cone",
+    Collisions = {Heroes=true, Minions=true, WindWall=true},
+    UseHitbox = true
+})
+local _E = Spell.Skillshot({
+    Slot = Enums.SpellSlots.E,
+    Range = math.huge,
+    Delay = 0.25,
+    Speed = 1600,
+    Radius = 500,
+    Type = "Linear",
+})
+local _R = Spell.Skillshot({
+    Slot = Enums.SpellSlots.R,
+    Range = math.huge,
+    Delay = 0.25,
+    Speed = 1600,
+    Radius = 130,
+    Type = "Linear",
+    Collisions = {Heroes=true, WindWall=true},
+    UseHitbox = true
+})
 
-local Logo = Renderer.CreateSprite("robur.png", Size.Logo.x, Size.Logo.y)
-local WarningSprite = Renderer.CreateSprite("warning.png", Size.Warning.x, Size.Warning.y)
+---@type TargetSelector
+local TS = _G.Libs.TargetSelector()
+local Ashe = {}
 
-local function DrawBackground()
-    Renderer.DrawRectOutline(Position.Border, Size.Border, 1, 2, 0x001A332F)
-    Renderer.DrawFilledRect(Position.Background, Size.Background, 1, 0x0D1B18FF)
+function Ashe.LoadMenu()
+    Menu.RegisterMenu("UnrulyAshe", "Unruly Ashe", function()
+        Menu.Separator("Unruly Ashe v".._version)
+        Menu.NewTree("Combo", "Combo Settings", function ()
+            Menu.Separator("Combo Settings")
+            Menu.Checkbox("Combo.UseQ", "Use [Q]", true)
+            Menu.Checkbox("Combo.UseW", "Use [W]", true)
+            Menu.Checkbox("Combo.UseE", "Use [E]", true)
+        end)
+
+        Menu.NewTree("Harass", "Harass Settings", function ()
+            Menu.Separator("Harass Settings")
+            Menu.Checkbox("Harass.UseQ", "Use [Q]", true)
+            Menu.Checkbox("Harass.UseW", "Use [W]", true)
+            Menu.Checkbox("Harass.UseE", "Use [E]", false)
+        end)
+
+        Menu.NewTree("Misc", "Misc Settings", function ()
+            Menu.Separator("Misc Settings")
+            Menu.Checkbox("Misc.AutoR", "Auto [R] Immobile/Dash", true)
+            Menu.Keybind("Misc.ForceR", "Force Ult Key", string.byte('T'))
+            Menu.Slider("Misc.MaxDistR", "Ult Max Distance", 1500, 400, 3000, 100)
+        end)
+
+        Menu.NewTree("Draw", "Draw Settings", function ()
+            Menu.Separator("Draw Settings")
+            Menu.Checkbox("Draw.W.Enabled", "Draw [W] Range", true)
+            Menu.ColorPicker("Draw.W.Color", "Draw [W] Color", 0x1CA6A1FF)
+            Menu.Checkbox("Draw.R.Enabled", "Draw [R] Range")
+            Menu.Checkbox("Draw.R.Damage", "Draw [R] Damage", true)
+            Menu.ColorPicker("Draw.R.Color", "Draw [R] Color", 0x0E1E6EFF)
+        end)
+        Menu.Separator("Author: Thorn")
+    end)
 end
 
-local function DrawLogo()
-    Logo:Draw(Position.Logo, 0, true)
+local function GameAvailable()
+    local gameAvailable = not (Game.IsChatOpen() or Game.IsMinimized())
+    return gameAvailable and not (Player.IsDead or Player.IsRecalling)
 end
 
-local function DrawWarning()
-    if math.floor(Game.GetTime() * 10) % 2 == 0 then
-        WarningSprite:Draw(Position.Warning, 0, true)
+function Ashe.OnTick(lagfree)
+    if not GameAvailable() then return end
+
+    _R.Range = Menu.Get("Misc.MaxDistR")
+    local mode = Orbwalker.GetMode()    
+
+    if Ashe.LogicR() then return end
+    if Orbwalker.CanCast() and (Ashe.LogicQ(mode) or Ashe.LogicW(mode)) then
+        return
     end
 end
 
-local function DrawLine()
-    Renderer.DrawLine(Position.Line.Start, Position.Line.End, 1, 0xBFA64BFF)
+function Ashe.LogicQ(_mode)
+    if Menu.Get(_mode .. ".UseQ", true) and _Q:IsReady() then
+        if TS:GetTarget(-1) then
+            return _Q:Cast()
+        end
+    end
 end
 
-local function DrawText()
-    Renderer.DrawText(Position.Text.Title, Size.Text.Title, Text.Title, 0xFF0000FF)
-    Renderer.DrawText(Position.Text.Subtitle, Size.Text.Subtitle, Text.Subtitle, 0x00FF00FF)
-    Renderer.DrawText(Position.Text.Message, Size.Text.Message, Text.Message, 0xFFD966FF)
+function Ashe.LogicW(_mode)
+    if Menu.Get(_mode .. ".UseW", true) and _W:IsReady() then
+        local target = _W:GetTarget()
+        if target then
+            return _W:CastOnHitChance(target, Enums.HitChance.Medium)
+        end
+    end
 end
 
-
-local function OnDraw()
-    DrawBackground()
-    DrawLogo()
-    DrawLine()
-    DrawText()
-    DrawWarning()
+function Ashe.LogicR()
+    if Menu.Get("Misc.ForceR") and _R:IsReady() then
+        local target = _R:GetTarget()
+        if target then
+            return _R:CastOnHitChance(target, 0.85)
+        end
+    end
 end
 
+function Ashe.AutoR(source)
+    if not (source.IsEnemy and Menu.Get("Misc.AutoR") and _R:IsReady()) then return end
 
-local function Initialize()
-    EventManager.RegisterCallback(Enums.Events.OnDraw, OnDraw)
+    local target = _R:GetTarget()
+    if target and target:CountEnemiesInRange(800) > 0 then
+        return _R:CastOnHitChance(target, Enums.HitChance.VeryHigh)
+    end
 end
-Initialize()
+
+function Ashe.OnDraw()
+    if Menu.Get("Draw.W.Enabled") then
+        Renderer.DrawCircle3D(Player.Position, _W.Range, 60, 2, Menu.Get("Draw.W.Color"))
+    end
+    if Menu.Get("Draw.R.Enabled") then
+        Renderer.DrawCircle3D(Player.Position, _R.Range, 25, 2, Menu.Get("Draw.R.Color"))
+    end
+end
+
+function Ashe.OnVisionLost(obj)
+    if Menu.Get(Orbwalker.GetMode() .. ".UseE", true) and _E:IsReady() then
+        local lastTarget = obj:Distance(Player) < TS:GetTrueAutoAttackRange(Player, obj) and Orbwalker.GetLastTarget()
+        if lastTarget and lastTarget == obj then
+            return _E:Cast(obj.Position)
+        end
+    end
+end
+
+function Ashe.OnGapclose(source, dashInst)
+    Ashe.AutoR(source)
+end
+
+function Ashe.OnHeroImmobilized(source, endT)
+    if Orbwalker.GetMode() == "Combo" then
+        Ashe.AutoR(source)
+    end
+end
+
+function Ashe.OnDrawDamage(target, dmgList)
+    if not Menu.Get("Draw.R.Damage") then return end
+    table.insert(dmgList, _R:GetDamage(target))
+end
+
+function OnLoad()
+    Ashe.LoadMenu()
+    for eventName, eventId in pairs(Enums.Events) do
+        if Ashe[eventName] then
+            EventManager.RegisterCallback(eventId, Ashe[eventName])
+        end
+    end
+    return true
+end
