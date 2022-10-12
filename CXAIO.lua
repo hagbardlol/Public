13402923286476d7dde0f91c1da10ac765cfa0bb
hagbardlol <59706487+hagbardlol@users.noreply.github.com>
2022-10-11 22:05:34 -0800

    Added All CXAIOChamps

--- CXAIO.lua
+++ CXAIO.lua
@@ -1,4 +1,4 @@
-local SCRIPT_NAME, VERSION, LAST_UPDATE = "CXAIO", "1.0.7", "10/08/2022"
+local SCRIPT_NAME, VERSION, LAST_UPDATE = "CXAIO", "1.0.8", "10/08/2022"
 _G.CoreEx.AutoUpdate("https://raw.githubusercontent.com/hagbardlol/Public/main/CXAIO.lua", VERSION)
 module(SCRIPT_NAME, package.seeall, log.setup)
 clean.module(SCRIPT_NAME, clean.seeall, log.setup)
@@ -9,6 +9,17 @@ local supportedChamp = {
     Zed = true,
     Cassiopeia = true,
     LeeSin = true,
+    Ahri = true,
+    Ashe = true,
+    Fizz = true,
+    Kassadin = true,
+    Kayn = true,
+    Nilah = true,
+    Rengar = true,
+    Sivir = true,
+    TahmKench = true,
+    Urgot = true,
+    Yone = true,
 }
 
 if supportedChamp[Player.CharName] then
