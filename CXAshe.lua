e1867cacf2e77f9a8c3ec19bd9c22120475c6ee7
hagbardlol <59706487+hagbardlol@users.noreply.github.com>
2022-10-19 01:30:55 -0800

    Removed CXAshe

--- CXAshe.lua
+++ CXAshe.lua
@@ -1,361 +1,85 @@
-local DreamTSLib = _G.DreamTS or require("DreamTS")
+if Player.CharName ~= "Ashe" then return end
 
----@type SDK_SDK
-local SDK = DreamTSLib.TargetSelectorSdk.SDK
+module("Coozbie Ashe", package.seeall, log.setup)
+clean.module("Coozbie Ashe", clean.seeall, log.setup)
+CoreEx.AutoUpdate("https://raw.githubusercontent.com/hagbardlol/Public/main/CXAshe.lua", "1.7")
+local CoreEx = _G.CoreEx
+local Enums, EventManager, Renderer, Game, Vector = CoreEx.Enums, CoreEx.EventManager, CoreEx.Renderer, CoreEx.Game, CoreEx.Geometry.Vector
+local Screen = Renderer.GetResolution()
 
----@type SDK_AIHeroClient
-local myHero = SDK.Player
+local Text = {
+    Title = "Unfortunately, this assembly is no longer supported by us.",
+    Subtitle = "No need to panic! We have made a new assembly available for you. Just check the forum or download the new assembly with the in-game downloader.",
+    Message = "Please disable this assembly in your loader and press F5 to make the message disappear!",
+}
 
-if myHero:GetCharacterName() ~= "Ashe" then return end
-
-local Ashe = {}
-
-local update_data = {
-    Robur = {
-        ScriptName = "CXAshe",
-        ScriptVersion = "1.6",
-        Repo = "https://raw.githubusercontent.com/hagbardlol/Public/main/"
+local Size = {
+    Border = Vector(800, 230), -- width, height
+    Background = Vector(799, 228),
+    Logo = Vector(350, 100),
+    Warning = Vector(40, 40),
+    Text = {
+        Title = Renderer.CalcTextSize(Text.Title),
+        Subtitle = Renderer.CalcTextSize(Text.Subtitle),
+        Message = Renderer.CalcTextSize(Text.Message),
     }
 }
 
-SDK.Common.AutoUpdate(update_data)
+local Position = {
+    Border = Vector((Screen.x / 2) - Size.Border.x / 2, (Screen.y / 2) - Size.Border.y / 2),
+    Background = Vector((Screen.x / 2) - Size.Background.x / 2, (Screen.y / 2) - Size.Background.y / 2),
+    Logo = Vector(Screen.x / 2, Screen.y / 2 - Size.Logo.y / 2),
+    Line = {
+        Start = Vector((Screen.x / 2 - Size.Border.x / 2) + 10, (Screen.y / 2 - Size.Logo.y / 2) + 60),
+        End =  Vector((Screen.x / 2 + Size.Border.x / 2) - 10, (Screen.y / 2 - Size.Logo.y / 2) + 60)
+    },
+    Text = {
+        Title = Vector((Screen.x / 2) - Size.Text.Title.x / 2, (Screen.y / 2 - Size.Logo.y / 2) + 60 + Size.Text.Title.y / 2),
+        Subtitle = Vector((Screen.x / 2) - Size.Text.Subtitle.x / 2, (Screen.y / 2 - Size.Logo.y / 2) + 60 + Size.Text.Title.y + Size.Text.Subtitle.y / 2),
+        Message = Vector((Screen.x / 2) - Size.Text.Message.x / 2, (Screen.y / 2 - Size.Logo.y / 2) + 60 + Size.Text.Title.y + Size.Text.Subtitle.y + Size.Text.Message.y / 2),
+    },
+    Warning = Vector((Screen.x / 2) - Size.Warning.x / 2, (Screen.y / 2 - Size.Logo.y / 2) + 60 + Size.Text.Title.y + Size.Text.Subtitle.y + Size.Text.Message.y + Size.Warning.y / 2 + 10)
+}
 
-local DreamTS = DreamTSLib.TargetSelectorSdk
-local Vector = SDK.Libs.Vector
-local roburTS = _G.Libs.TargetSelector()
-local HealthPred = Libs.HealthPred
+local Logo = Renderer.CreateSprite("robur.png", Size.Logo.x, Size.Logo.y)
+local WarningSprite = Renderer.CreateSprite("warning.png", Size.Warning.x, Size.Warning.y)
 
----@param objects SDK_GameObject[]
----@return SDK_AIHeroClient[]
-local function GetHerosFromObjects(objects)
-    local res = {}
-    for i, obj in ipairs(objects) do
-        res[i] = obj:AsHero()
-    end
-    return res
+local function DrawBackground()
+    Renderer.DrawRectOutline(Position.Border, Size.Border, 1, 2, 0x001A332F)
+    Renderer.DrawFilledRect(Position.Background, Size.Background, 1, 0x0D1B18FF)
 end
 
-local enemies = GetHerosFromObjects(SDK.ObjectManager:GetEnemyHeroes())
-
-function Ashe:__init()
-    self.w = {
-        type = "linear",
-        speed = 1500,
-        range = 1200,
-        delay = 0.25,
-        width = 120,
-        collision = {
-            ["Wall"] = true,
-            ["Hero"] = true,
-            ["Minion"] = true
-        }
-    }
-    self.r = {
-        type = "linear",
-        speed = 1600,
-        range = 2000,
-        delay = 0.25,
-        width = 240,
-        collision = {
-            ["Wall"] = true,
-            ["Hero"] = true,
-            ["Minion"] = false
-        }
-    }
-    self:Menu()
-    self.TS =
-        DreamTS(
-        self.menu:GetLocalChild("dreamTs"),
-        {
-            Damage = DreamTS.Damages.AD
-        }
-    )
-    SDK.EventManager:RegisterCallback(SDK.Enums.Events.OnTick, function() self:OnTick() end)
-    SDK.EventManager:RegisterCallback(SDK.Enums.Events.OnDraw, function() self:OnDraw() end)
-    _G.CoreEx.EventManager.RegisterCallback(_G.CoreEx.Enums.Events.OnPostAttack, function(target) self:OnExecuteCastFrame(SDK.Types.AIBaseClient(target)) end)
+local function DrawLogo()
+    Logo:Draw(Position.Logo, 0, true)
 end
 
-function Ashe:Menu()
-    self.menu = SDK.Libs.Menu("cxashe", "Cyrex Ashe")
-
-    self.menu
-    :AddLabel("Cyrex Ashe Settings", true)
-    :AddSubMenu("dreamTs", "Target Selector")
-
-    self.menu
-    :AddSubMenu("combo", "Combo Settings")
-        :AddLabel("Q Settings", true)
-        :AddCheckbox("q", "Use Q", true)
-        :AddLabel("W Settings", true)
-        :AddCheckbox("w", "Use W")
-        :GetParent()
-
-    :AddSubMenu("r", "Ultimate Settings")
-        :AddLabel("R Combo Settings", true)
-        :AddCheckbox("rm", "R if Enemy in Melee", true)
-        :AddLabel("Semi-Manual R Settings", true)
-        :AddCheckbox("r", "Use R", true)
-        :AddKeybind("rt", "Use Semi Manual R", string.byte("T"))
-        :AddSubMenu("bl", "Black List")
-
-    local r_menu = self.menu:GetLocalChild("r.bl"):AsMenu()
-
-    for _, enemy in ipairs(enemies) do
-        r_menu:AddCheckbox(enemy:GetCharacterName(), "Do Not Hit: " .. enemy:GetCharacterName(), false)
-    end
-
-    local rm = self.menu:GetLocalChild("r"):AsMenu()
-    rm:AddLabel("Anti-Gap Settings", true)
-    rm:AddSubMenu("ag", "Anti Gapclose")
-
-    local anti_gap_menu = self.menu:GetLocalChild("r.ag"):AsMenu()
-    self.antiGapHeros = {}
-    for _, hero in ipairs(enemies) do
-        local char_name = hero:GetCharacterName()
-        anti_gap_menu:AddCheckbox(char_name, "AntiGap:" .. char_name, true)
-        self.antiGapHeros[hero:GetNetworkId()] = true
-    end
-
-    self.menu
-    :AddSubMenu("harass", "Harass Settings")
-        :AddLabel("W Settings", true)
-        :AddCheckbox("w", "Use W", true)
-        :GetParent()
-    :AddSubMenu("auto", "Automatic Settings")
-        :AddLabel("Killsteal Settings", true)
-        :AddCheckbox("uwks", "Use W in Killsteal", true)
-        :AddCheckbox("urks", "Use R in Killsteal", true)
-        :GetParent()
-    :AddSubMenu("draws", "Draw")
-        :AddCheckbox("w", "W", true)
-        :GetParent()
-        :AddLabel("Version: " .. update_data.Robur.ScriptVersion .. "", true)
-        :AddLabel("Author: Coozbie", true)
-
-    self.menu:Render()
-end
-
-
-local color_white = SDK.Libs.Color.GetD3DColor(255,7,141,237)
-
-function Ashe:OnDraw()
-    if not myHero:IsOnScreen() then
-        return
-    end
-
-    if self.menu:GetLocal("draws.w") and myHero:CanUseSpell(SDK.Enums.SpellSlot.W) then
-        SDK.Renderer:DrawCircle3D(myHero:GetPosition(), self.w.range, color_white)
-    end    
-end
-
-local delayedActions, delayedActionsExecuter = {}, nil
-function Ashe:DelayAction(func, delay, args) --delay in seconds
-    if not delayedActionsExecuter then
-        function delayedActionsExecuter()
-            for t, funcs in pairs(delayedActions) do
-                if t <= os.clock() then
-                    for i = 1, #funcs do
-                        local f = funcs[i]
-                        if f and f.func then
-                            f.func(unpack(f.args or {}))
-                        end
-                    end
-                    delayedActions[t] = nil
-                end
-            end
-        end
-        SDK.EventManager:RegisterCallback(SDK.Enums.Events.OnTick, delayedActionsExecuter)
-    end
-    local t = os.clock() + (delay or 0)
-    if delayedActions[t] then
-        delayedActions[t][#delayedActions[t] + 1] = {func = func, args = args}
-    else
-        delayedActions[t] = {{func = func, args = args}}
+local function DrawWarning()
+    if math.floor(Game.GetTime() * 10) % 2 == 0 then
+        WarningSprite:Draw(Position.Warning, 0, true)
     end
 end
 
-function Ashe:GetPercentHealth(obj)
-    obj = obj or myHero
-    return obj:GetHealthPercent()
+local function DrawLine()
+    Renderer.DrawLine(Position.Line.Start, Position.Line.End, 1, 0xBFA64BFF)
 end
 
-function Ashe:GetTotalAP(obj)
-  local obj = obj or myHero
-  return obj:GetTotalAP()
+local function DrawText()
+    Renderer.DrawText(Position.Text.Title, Size.Text.Title, Text.Title, 0xFF0000FF)
+    Renderer.DrawText(Position.Text.Subtitle, Size.Text.Subtitle, Text.Subtitle, 0x00FF00FF)
+    Renderer.DrawText(Position.Text.Message, Size.Text.Message, Text.Message, 0xFFD966FF)
 end
 
-function Ashe:MoveToMouse()
-    SDK.Input:MoveTo(SDK.Renderer:GetMousePos3D())
+
+local function OnDraw()
+    DrawBackground()
+    DrawLogo()
+    DrawLine()
+    DrawText()
+    DrawWarning()
 end
 
-function Ashe:TotalAD(obj)
-    obj = obj or myHero
-    return obj:GetTotalAD()
+
+local function Initialize()
+    EventManager.RegisterCallback(Enums.Events.OnDraw, OnDraw)
 end
-
----@param obj SDK_AIBaseClient | nil
-function Ashe:GetBonusAD(obj)
-  obj = obj or myHero
-  return obj:GetFlatPhysicalDamageMod()
-end
-
-function Ashe:GetDistanceSqr(p1, p2)
-    p2 = p2 or myHero:GetPosition()
-    local dx = p1.x - p2.x
-    local dz = p1.z - p2.z
-    return dx*dx + dz*dz
-end
-
-function Ashe:ValidTarget(object, distance) 
-    return object and object:IsValid() and object:IsEnemy() and object:IsVisible() and not object:GetBuff('SionPassiveZombie') and not object:GetBuff('FioraW') and object:IsAlive() and not object:IsInvulnerable() and (not distance or  object:GetPosition():DistanceSqr(myHero:GetPosition()) <= distance * distance)
-end
-
-function Ashe:GetAARange(target)
-    return myHero:GetAttackRange() + myHero:GetBoundingRadius() + (target and target:GetBoundingRadius() or 0)
-end
-
-function Ashe:wDmg(target)
-    if myHero:CanUseSpell(SDK.Enums.SpellSlot.W) then
-        local wDamage = (5 + (15 * myHero:GetSpell(SDK.Enums.SpellSlot.W):GetLevel()) + (self:TotalAD() * 1))
-        return self.TS.CalcDmg(myHero, target:AsAI(), wDamage, 0, 0)
-    end
-end
-
-function Ashe:rDmg(target)
-    if myHero:CanUseSpell(SDK.Enums.SpellSlot.R) then
-        local rDamage = ((200 * myHero:GetSpell(SDK.Enums.SpellSlot.R):GetLevel())) + (self:GetTotalAP() * 1)
-        return self.TS.CalcDmg(myHero, target:AsAI(), rDamage, 0, 0)
-    end
-end
-
-function Ashe:GetEnemyHeroesInRange(range, pos)
-    local pos = pos or myHero:GetPosition()
-    local h = {}
-    local enemies = SDK.ObjectManager:GetEnemyHeroes()
-    for i = 1, #enemies do
-        local hero = enemies[i]:AsAI()
-        if hero and hero:IsEnemy() and not hero:IsInvulnerable() and hero:IsAlive() and hero:IsVisible() and hero:IsTargetable() and hero:GetPosition():DistanceSqr(pos) < range * range then
-            h[#h + 1] = hero
-        end
-    end
-    return h
-end
-
-function Ashe:CastW(pred)
-    if pred.rates["slow"] then
-        SDK.Input:Cast(SDK.Enums.SpellSlot.W, pred.castPosition)
-        pred:Draw()
-        return true
-    end
-end
-
-function Ashe:CastR(pred, rate)
-    rate = rate or "slow"
-    if pred.rates[rate] then
-        SDK.Input:Cast(SDK.Enums.SpellSlot.R, pred.castPosition)
-        pred:Draw()
-        return true
-    end
-end
-
-function Ashe:OnExecuteCastFrame(target)
-    if self.menu:GetLocal("combo.q") and myHero:CanUseSpell(SDK.Enums.SpellSlot.Q) and (_G.Libs.Orbwalker.GetMode() == "Combo") then
-        if target and roburTS:IsValidTarget(target.data) and target:GetPosition():Distance(myHero:GetPosition()) < self:GetAARange(target) then
-            SDK.Input:Cast(SDK.Enums.SpellSlot.Q, myHero)
-        end
-    end
-end
-
-function Ashe:OnTick()
-    local ComboMode = _G.Libs.Orbwalker.GetMode() == "Combo"
-    local HarassMode = _G.Libs.Orbwalker.GetMode() == "Harass"
-    local WaveclearMode = _G.Libs.Orbwalker.GetMode() == "Waveclear"
-
-    if myHero:CanUseSpell(SDK.Enums.SpellSlot.W) then
-        local w_targets, w_preds = self.TS:GetTargets(self.w, myHero:GetPosition())
-        local w_ks, w_ks_pred = self.TS:GetTargets(self.w, myHero:GetPosition(), function(enemy) return self:wDmg(enemy) >= enemy:GetHealth() end)
-
-        if (ComboMode and self.menu:GetLocal("combo.w") and not myHero:GetBuff("AsheQAttack")) or (HarassMode and self.menu:GetLocal("harass.w") and not myHero:GetBuff("AsheQAttack")) then
-            local target = w_targets[1]
-            if target then
-                local pred = w_preds[target:GetNetworkId()]
-                if pred and self:CastW(pred) then
-                    return
-                end
-            end
-        end
-        if self.menu:GetLocal("auto.uwks") then
-            local target = w_ks[1]
-            if target then
-                local pred = w_ks_pred[target:GetNetworkId()]
-                if pred and self:CastW(pred) then
-                    return
-                end
-            end
-        end
-    end
-    if myHero:CanUseSpell(SDK.Enums.SpellSlot.R) then
-        local r_targets, r_preds = self.TS:GetTargets(self.r, myHero:GetPosition(), nil, nil, self.TS.Modes["Closest To Mouse"])
-        local r_ks, r_ks_pred = self.TS:GetTargets(self.r, myHero:GetPosition(), function(enemy) return self:rDmg(enemy) >= enemy:GetHealth() end)
-
-        if (ComboMode and self.menu:GetLocal("r.rm")) then
-            local target = r_targets[1]
-            if target then
-                local pred = r_preds[target:GetNetworkId()]
-                if pred and self:GetDistanceSqr(target:GetPosition()) < 300^2 and self:CastR(pred, "instant") then
-                    return
-                end
-            end
-        end
-        if (self.menu:GetLocal("r.rt") and self.menu:GetLocal("r.r")) then
-            local target = r_targets[1]
-            if target then
-                local pred = r_preds[target:GetNetworkId()]
-                if pred and not self.menu:GetLocal("r.bl." .. target:GetCharacterName()) and self:CastR(pred, "slow") then
-                    return
-                end
-            end
-        end
-        if self.menu:GetLocal("auto.urks") then
-            local target = r_ks[1]
-            if target then
-                local pred = r_ks_pred[target:GetNetworkId()]
-                if pred and self:CastR(pred) then
-                    return
-                end
-            end
-        end
-        for i = 1, #r_targets do
-            local unit = r_targets[i]
-            local pred = r_preds[unit:GetNetworkId()]
-            if pred then
-                if pred.targetDashing and self.antiGapHeros[unit:GetNetworkId()] and self.menu:GetLocal("r.ag." .. unit:GetCharacterName()) and self:CastR(pred) then
-                    return
-                end
-                if pred.isInterrupt and self:CastR(pred) then
-                    return
-                end
-            end
-        end
-    end
-end
-
-local get_d3d_color = SDK.Libs.Color.GetD3DColor
-function Ashe:Hex(a, r, g, b)
-    return get_d3d_color(a, r, g, b)
-end
-
-function Ashe:GetTargetNormal(dist, all)
-    local res = self.TS:update(function(unit) return _G.Prediction.SDK.IsValidTarget(unit, dist) end)
-    if all then
-        return res
-    else
-        if res and res[1] then
-            return res[1]
-        end
-    end
-end
-
-if myHero:GetCharacterName() == "Ashe" then
-    Ashe:__init()
-end
+Initialize()
